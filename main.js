4c1342c8512b584cb0cc92dc337c79ab87e9a0b7
vagnerNatividade <vagnerflorindojunior@gmail.com>
2023-10-24 09:56:07 -0300

    Aula 9 - Aplicando a lógica

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
 function handleClick(event) {
   event.preventDefault();
 
-  const inNumber = document.querySelector("#inNumber");
-  console.log(inNumber.value);
+  const inNumber = document.getElementById("inNumber");
+
+  if (Number(inNumber.value) == randomNumber) {
+    document.querySelector(".screen1").classList.add("hide");
+    document.querySelector(".screen2").classList.remove("hide");
+
+    document.querySelector(
+      "h2"
+    ).innerHTML = `Você acertou em ${xAttempts} tentativas`;
+  }
+
+  inNumber.value = "";
+  inNumber.focus();
+  xAttempts++;
 }
